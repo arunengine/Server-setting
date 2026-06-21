2fc9a8feb11bc98576eb6f0ea9525163a6d1810e
arunengine <iarunarumugam@gmail.com>
2026-06-21 15:02:05 +0530

    3rd commit

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ import dotenv from "dotenv"
 import userRoute from "./routes/userRoutes.js";
 
 
+dotenv.config();
+
 const app = express(); 
 app.use(cors())
 
-app.use(express.json()); 
+app.use(express.json());
 
+const PORT = process.env.PORT || 3000
 
 app.use("/api/users" , userRoute )
 
-app.listen(3000 , ()=>{
-    console.log("server is running on port number 3000");
+app.listen(PORT , ()=>{
+    console.log(`server is running on port number 3000${PORT}`);
     
 }) 
